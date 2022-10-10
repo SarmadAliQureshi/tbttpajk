58f6cd10038f1aae137ca0b5b88a0655c23cf49e
SarmadAliQureshi <sarmadalikaharian@gmail.com>
2022-10-11 02:13:48 +0500

    cors added

--- connection.js
+++ connection.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 // console.log('abc',process.env.NODE_ENV);
 
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-// const connectionString = 'postgresql://postgres:postgres@database-1.c6am0oww5fuv.ap-northeast-1.rds.amazonaws.com:5432/postgres'
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = 'postgresql://postgres:postgres@database-1.c6am0oww5fuv.ap-northeast-1.rds.amazonaws.com:5432/postgres'
 
 
 // const pool = new Pool({
